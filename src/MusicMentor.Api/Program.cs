b24fd53a8abfef68c9ee6a604e172cfe8aebd002
raminsolhi <raminsolhi@gmail.com>
2026-07-25 22:35:44 +0330

    activating swagger just for test

--- src/MusicMentor.Api/Program.cs
+++ src/MusicMentor.Api/Program.cs
@@ -52,11 +52,11 @@ using (var scope = app.Services.CreateScope())
 }
 
 // --- Middleware pipeline ---
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("Default");
 
